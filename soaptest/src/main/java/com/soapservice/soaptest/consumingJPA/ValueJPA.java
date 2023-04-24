1ace5d2f76324f7254a9c33191ee5d1ba44afd96
Adrián Bedón <82057915+AdrianBedon@users.noreply.github.com>
2023-04-24 12:39:56 -0500

    Delete soaptest directory

--- soaptest/src/main/java/com/soapservice/soaptest/consumingJPA/ValueJPA.java
+++
@@ -1,6 +0,0 @@
-package com.soapservice.soaptest.consumingJPA;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record ValueJPA (String name, String lastname){ }
